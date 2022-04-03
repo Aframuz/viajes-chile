384a6eaef91005fa98a8d602dc1a0e50eab60961
aframuz <aframuz@tutanota.io>
2022-04-02 23:29:03 -0400

    added jQuery, fixed navbar position depending on windows size

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,22 @@
-// Tests
-console.log("Linked");
+$(function () {
+   // variables
+   var win = $(this)
+
+   /*----------  navbar behavior  ----------*/
+   // First windows size check
+   if (win.width() > 576) {
+      // add class to navbar
+      $(".navbar").addClass("fixed-top")
+   }
+
+   // Media Query windows resize sm(576px)
+   $(window).on("resize", function () {
+      if (win.width() > 576) {
+         // add class to navbar
+         $(".navbar").addClass("fixed-top")
+      } else {
+         // remove class to navbar
+         $(".navbar").removeClass("fixed-top")
+      }
+   })
+})
